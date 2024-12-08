bc0259940d784c0980bc1aa017a8e23c29605656
Daniel Gladstone <gladstone.d@gmail.com>
2024-12-08 12:37:35 -0500

    move api

--- app/api/health-check.js
+++ app/api/health-check.js
@@ -1,6 +1,7 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default async (req: VercelRequest, res: VercelResponse) => {
+  console.log("Health check endpoint hit!");
   if (req.method !== "GET") {
     res.status(405).json({ error: "Method Not Allowed" });
     return;
