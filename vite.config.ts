1e85422ad4b736fde87ea6ee6a34424bd5fed71f
Daniel Gladstone <gladstone.d@gmail.com>
2024-12-08 19:44:45 -0500

    working

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Output directory
+    emptyOutDir: true, // Clears the output directory before building
+    rollupOptions: {
+      input: './index.html', // Ensure Vite uses the correct input file
+    },
+  },
+  base: '/', // Public path for assets
+});
