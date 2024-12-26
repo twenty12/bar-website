61409538d9d031d8ac32ae7db17f3fcae6411ed7
Daniel Gladstone <gladstone.d@gmail.com>
2024-12-26 10:27:34 -0700

    freshen event page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "antd";
+import { Col, Row, Typography } from "antd";
 import React from "react";
 
 
@@ -43,13 +43,29 @@ const Home: React.FC = () => {
           </div>
         </div>
       </div>
-      <div className="full-page-video-body" style={{ marginBottom: '60px' }}>
-        <Typography.Title level={1} style={{ fontWeight: 300, margin: 0 }}>
-          Since 2016, Honey's has welcomed creative communities of all kinds to the corner of Scott and Randolph. We're a bar and event space that transforms the city's raw, artistic energy into thoughtful experiences for our guests. From poetry readings to floral markets to wild dance parties, a unique experience awaits with each visit. 
-          <br />
-          <br />
-          Through carefully curated flavors, sounds, and movements, we ensure every guest leaves feeling both energized and cared for.
-        </Typography.Title>
+      <div className="full-page-video-body" style={{
+        marginBottom: '60px',
+        justifyContent: 'center',
+        display: 'flex'
+      }}>
+        <Row
+          style={{maxWidth: '990px'}}
+          gutter={[16, 16]}
+        >
+          <Col xs={24} md={4} style={{ textAlign: 'left' }}>
+            <Typography.Title level={3}>
+              About Us
+            </Typography.Title>
+          </Col>
+          <Col xs={24} md={20}>
+            <Typography.Title level={3} style={{ fontWeight: 300 }}>
+              Since 2016, Honey's has welcomed creative communities of all kinds to the corner of Scott and Randolph. We're a bar and event space that transforms the city's raw, artistic energy into thoughtful experiences for our guests. From poetry readings to floral markets to wild dance parties, a unique experience awaits with each visit.
+              <br />
+              <br />
+              Through carefully curated flavors, sounds, and movements, we ensure every guest leaves feeling both energized and cared for.
+            </Typography.Title>
+          </Col>
+        </Row>
         <br />
         <br />
         <br />
