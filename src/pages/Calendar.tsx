ac6121d6cd2e561f9f4a36b34b206f404a0d17fe
Daniel Gladstone <gladstone.d@gmail.com>
2025-01-11 11:10:36 -0500

    add banner

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { Row, Typography } from "antd";
+import { Grid, Row, Typography } from "antd";
 
 import EventCard from "../components/eventCard";
 import FullPageSpin from "../components/fullPageSpin";
 import { useNotionDB } from "../providers/CalendarProvider";
 // import StayAdvisedForm from "../components/stayAdvised";
+const { useBreakpoint } = Grid;
 
 const Calendar: React.FC = () => {
   const { events, loading } = useNotionDB();
-
+  const screens = useBreakpoint();
+  const isBelowMd = !screens.md;
   if (loading) {
     return <FullPageSpin />;
   }
@@ -30,6 +32,35 @@ const Calendar: React.FC = () => {
         </Typography.Title>
         {/* <StayAdvisedForm /> */}
       </Row>
+      <div
+        style={{
+          backgroundColor: "white", // Dark background
+          margin: "5px 0", // Spacing between performers
+          marginLeft: isBelowMd ? "5px" : "auto",
+          marginRight: isBelowMd ? "15px" : "auto",
+          color: "white", // Light text for contrast
+          border: "2px solid rgba(255, 121, 0, 0.5)",
+          borderRadius: "4px", // Slightly more rounded corners
+          padding: "10px",
+          width: isBelowMd ? "auto" : "70%", // Full width
+          display: "flex",            // Flexbox layout
+          flexDirection: "column",    // Stack children vertically
+        }}
+      >
+        <Typography.Title
+          level={4}
+          style={{ marginTop: "0px", marginBottom: "0px", textAlign: "center", fontWeight: 300 }}
+        >
+          🏗️ 🥶 Event Space Renovations in Progress 🥶 🏗️ <br />
+        </Typography.Title>
+        <Typography.Title
+          level={5}
+          style={{ marginTop: "15px", marginBottom: "0px", fontWeight: 300 }}
+        >
+          We're taking advantage of the winter season to renovate our space with a major audio upgrade and new bar additions.
+          Things will be a little quieter this winter. <br/><br/>We'll be full force in spring with a stacked lineup of parties. <br />
+        </Typography.Title>
+      </div>
       <div>
         {events.map((event) => (
           <EventCard key={event.id} event={event} />
