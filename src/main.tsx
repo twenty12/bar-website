6ccebf59959374db2124e31961072f9d22e0e22c
Daniel Gladstone <gladstone.d@gmail.com>
2025-04-05 10:46:20 -0400

    update with sentry

--- src/main.tsx
+++ src/main.tsx
@@ -9,21 +9,29 @@ import { NotionDBProvider } from "./providers/CalendarProvider";
 import { HelmetProvider } from "react-helmet-async";
 import { GalleryProvider } from "./providers/GalleryProvider";
 import { WebsiteCopyProvider } from "./providers/websiteCopyProvider";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://12cd852129ca17573b42d88728d9f7b1@o4509101155483648.ingest.us.sentry.io/4509101177831424"
+});
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <NotionDBProvider>
-        <HelmetProvider>
-          <ConfigProvider>
-            <GalleryProvider>
-              <WebsiteCopyProvider>
-                <App />
-              </WebsiteCopyProvider>
-            </GalleryProvider>
-          </ConfigProvider>
-        </HelmetProvider>
-      </NotionDBProvider>
-    </BrowserRouter>
+    <Sentry.ErrorBoundary>
+      <BrowserRouter>
+        <NotionDBProvider>
+          <HelmetProvider>
+            <ConfigProvider>
+              <GalleryProvider>
+                <WebsiteCopyProvider>
+                  <App />
+                </WebsiteCopyProvider>
+              </GalleryProvider>
+            </ConfigProvider>
+          </HelmetProvider>
+        </NotionDBProvider>
+      </BrowserRouter>
+    </Sentry.ErrorBoundary>
   </React.StrictMode>
 );
